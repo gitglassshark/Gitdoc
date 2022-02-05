442508f71e82e6b190338f6f9cdcb77006a3b392
Hluk <68088825@qq.com>
2022-02-05 18:39:27 +0800

    add and commit

---
+++ s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git add *
+git cm "add and commit"
+echo -e "stat git:\n"
+git s -b --show-stash --short
+echo -e "ls git files:\n"
+git ls
+echo -e "ls git branch stat\n"
+git lb
+echo -e "ls current diretory file:\n"
+ls
