4424821f9ed118b90b2808f5d27ba02626dca049
Hluk <68088825@qq.com>
2022-02-05 18:49:22 +0800

    add and commit

--- s.sh
+++ s.sh
@@ -9,6 +9,9 @@ echo -e "ls git branch stat\n"
 git lb
 echo -e "ls current diretory file:\n"
 ls
+git rtup
 git remote -v
-git remote -v;git rtup;git ss;git branch -av;git  status -s ;git log --oneline -10
+git ss
+git branch -av
+git log --oneline -10
 git push -v
