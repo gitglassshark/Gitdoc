a21847f14a792d37ab092095ad75ba598923c618
Hluk <68088825@qq.com>
2022-02-06 00:32:34 +0800

    add and commit

--- cprimer/one/fun.cpp
+++ cprimer/one/fun.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 #include "fun.hpp"
+
+int ex_i=0;
+int ex_y=0;
+
 int null(int i)
 {
     int k=i;
--- cprimer/one/main.cpp
+++ cprimer/one/main.cpp
@@ -7,10 +7,15 @@ using namespace std;
 
 int main()
 {
-    int t;
-    string msg ="Start! ......\n";
-    t=sizeof(msg);
-    cout << msg;
+    int t=0;
+    int x=100;
+    //int x_unuse=99;
+    string start_msg ="Start! ......\n";
+    string end_msg="End! ......\n";
+    t=sizeof(start_msg);
+    cout << start_msg;
     cout << null(t)<<endl;
+    cout << null(x)<<endl;
+    cout << end_msg;
     return 0;
 }
