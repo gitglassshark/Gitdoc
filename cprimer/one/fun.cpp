110a6b1ab991f9d2baa7eda5bb16dc8edf4a7938
Hluk <68088825@qq.com>
2022-02-05 23:35:22 +0800

    add and commit

---
+++ cprimer/one/fun.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "fun.hpp"
+int null(int i)
+{
+    int k=i;
+    return k;
+}
---
+++ cprimer/one/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "fun.hpp"
+
+using namespace std;
+
+int main()
+{
+    int t;
+    string msg ="Start! ......\n";
+    t=sizeof(msg);
+    cout << msg;
+    cout << null(t)<<endl;
+    return 0;
+}
